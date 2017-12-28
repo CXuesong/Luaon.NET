a006c2fa23af4fec64251eebc2e1ee4ec935bd33
forest93 <forest93@163.com>
2017-12-28 20:00:34 +0800

    Add test project.

---
+++ XUnitTestProject1/LuaConvertTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Luaon;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class LuaConvertTests
+    {
+        [Fact]
+        public void ToStringTest()
+        {
+            Assert.Equal(@"""test\a\b\r\n\""abc""", LuaConvert.ToString("test\a\b\r\n\"abc"));
+            Assert.Equal(@"'abc""def'", LuaConvert.ToString("abc\"def", "'"));
+            Assert.Equal(@"[[test]]", LuaConvert.ToString("test", "[["));
+            Assert.Equal(@"[==[test]==]", LuaConvert.ToString("test", "[==["));
+            Assert.Equal(@"[==========[test]==========]", LuaConvert.ToString("test", "[==========["));
+            Assert.Equal("-12345", LuaConvert.ToString(-12345));
+            Assert.Equal("123.45", LuaConvert.ToString(123.45));
+            Assert.Equal(@"""http://cxuesong.com/""", LuaConvert.ToString(new Uri("http://cxuesong.com/")));
+            Assert.Equal("nil", LuaConvert.ToString((object)null));
+            Assert.Equal("-12345", LuaConvert.ToString((object)-12345));
+            Assert.Equal("123.45", LuaConvert.ToString((object)123.45));
+        }
+    }
+}
